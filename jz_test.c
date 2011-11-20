53a4ac6ad9343a7e782bfb9231636418eba59af0
Jackson Zhou <jacksonzhou.mail@gmail.com>
2011-11-20 15:45:59 -0600

    adding external oracle driver to ext

--- jz_test.c
+++ jz_test.c
@@ -1,12 +1,13 @@
 int j = 3;
 int k = 3 + 3;
+int l = j;
+
 int main ( ) {
     int main2 = test(3);
-    showstack "break 2";
     return 0;
 }
 
 int test ( int x ) {
-    showstack "hello world";
+    showstack "my break";
     return x;
 }
